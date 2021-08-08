52067fa19d9d638e935e0bfb8331780a2574df82
StevenSong <steven.song98@gmail.com>
2021-08-07 21:13:56 -0500

    feedback link, back buttons, canvas login step

--- walkthrough.js
+++ walkthrough.js
@@ -1,3 +1,20 @@
+function clearErrors() {
+  let errorBox = document.getElementById("error-box")
+  errorBox.innerHTML = ""
+}
+
+function showCoursePrompt() {
+  clearErrors()
+  let canvasLoginDiv = document.getElementById("canvas-login")
+  let getCourseListDiv = document.getElementById("get-course-list")
+  canvasLoginDiv.classList.toggle("hidden")
+  getCourseListDiv.classList.toggle("hidden")
+}
+
+function undoShowCoursePrompt() {
+  showCoursePrompt()
+}
+
 function copyToClipboard(link, course) {
   let input = document.createElement("input")
   input.value = link
@@ -9,9 +26,8 @@ function copyToClipboard(link, course) {
   alert("Copied link for " + course + " to clipboard")
 }
 
-function getCourseList() {
-  let errorBox = document.getElementById("error-box")
-  errorBox.innerHTML = ""
+function showCourseLinks() {
+  clearErrors()
   try {
     let courses = JSON.parse(document.getElementById("paste-box").value)
     if (courses.length == 0) {
@@ -28,11 +44,14 @@ function getCourseList() {
         let course = courses[i]
         row = table.insertRow(-1)
         nameCell = row.insertCell(0)
-        nameCell.classList.add("name-col")
         linkCell = row.insertCell(1)
-        linkCell.classList.add("link-col")
         copyCell = row.insertCell(2)
+        nameCell.classList.add("name-col")
+        linkCell.classList.add("link-col")
         copyCell.classList.add("copy-col")
+        nameCell.classList.add("generated")
+        linkCell.classList.add("generated")
+        copyCell.classList.add("generated")
         let courseName = "Unknown"
         if ("name" in course) {
           courseName = course.name
@@ -56,4 +75,18 @@ function getCourseList() {
     console.log(error)
     errorBox.innerHTML= "The pasted text doesn't look right, could you try clearing the text box and copying and pasting again?"
   }
+}
+
+function undoShowCourseLinks() {
+  clearErrors()
+  let pasteBox = document.getElementById("paste-box")
+  pasteBox.value = ""
+  const cells = document.getElementsByClassName("generated");
+  while(cells.length > 0){
+    cells[0].parentNode.removeChild(cells[0]);
+  }
+  let getCourseListDiv = document.getElementById("get-course-list")
+  let courseListLinksDiv = document.getElementById("course-list-links")
+  getCourseListDiv.classList.toggle("hidden")
+  courseListLinksDiv.classList.toggle("hidden")
 }
